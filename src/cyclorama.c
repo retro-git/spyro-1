48a6213d7bd54e6add99eefab10faaf15330e77b
retro <43223593+retro-git@users.noreply.github.com>
2025-08-27 15:30:01 +0800

    test

--- src/cyclorama.c
+++ src/cyclorama.c
@@ -1,3 +1,13 @@
 #include "common.h"
+#include <libgte.h>
+#include "vector.h"
+#include "memory.h"
+#include "math.h"
+#include "images.h"
+#include "camera.h"
+#include "cyclorama.h"
+#include "environment.h"
+
+void func_80017B48(int *param_1,int *param_2,uint param_3);
 
 INCLUDE_ASM("asm/nonmatchings/cyclorama", func_80050BD0);
